32ef207d2c5032360e3e42aefed884766e48bfdf
Adam Koza <adamkoza@Adams-MacBook-Pro.local>
2014-06-23 14:43:48 -0400

    Add json encoding functionality to select.php

--- select.php
+++ select.php
@@ -1,8 +1,20 @@
 <?php
 
-    $db = new PDO("mysql:host=$dbserver;dbname=$dbname", $dbusername, $dbpassword);
+    $db = new PDO("mysql:host=$dbserver;dbname=$dbname", $dbusername,
+                  $dbpassword);
 
-    $query = "SELECT * FROM $table;";
+    $sql = "SELECT * FROM $table;";
+    $result = $db->query($sql);
+
+    // Fetch and encode the table into json format
+    $json_response = array();
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $json_response[] = $row;
+    }
+    echo json_encode($json_response);
+
+    // Close the connection
+    $db = null;
 
 ?>
 
