808e136b90b05580d3386a1224733c7b1d525d2a
Adam Koza <adamkoza@Adams-MacBook-Pro.local>
2014-06-23 12:38:37 -0400

    Make lines span 80 chars or less

--- main.php
+++ main.php
@@ -14,7 +14,8 @@ $request = substr($request, $pos + strlen($basepath));
 // Split the request URI into an array based on forward slashes
 $requestArray = split("/", $request);
 
-// Split the second item in the array to separate the table name from the filetype (There's probably a better way to do this)
+// Split the second item in the array to separate the table name from the
+// filetype (There's probably a better way to do this)
 $requestArray[1] = split(".", $requestArray[1]);
 
 // Set variables based on the request URI
@@ -22,7 +23,9 @@ $dbname = $requestArray[0];
 $table = $requestArray[1][0];
 $foramt = $requestArray[1][1];
 
-// Run the code for the SQL select statement. Later, we'll want to add logic here to determine which SQL statement is being run instead of just assuming SELECT.
+// Run the code for the SQL select statement. Later, we'll want to add logic
+// here to determine which SQL statement is being run instead of just assuming
+// SELECT
 include("select.php");
 
 ?>
