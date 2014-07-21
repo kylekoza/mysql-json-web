5132f5cb19cf2b654052aafef76a90bd12b9046b
Adam Koza <adamkoza@dept-143-215-32-68.vpn.gatech.edu>
2014-07-21 15:25:52 -0400

    Add post option for WHERE and ORDER BY

--- select.php
+++ select.php
@@ -3,7 +3,16 @@
     $db = new PDO("mysql:host=$dbserver;dbname=$dbname", $dbusername,
                   $dbpassword);
 
-    $sql = "SELECT * FROM $table;";
+    if (isset($_POST['WHERE'])) {
+        if (isset($_POST['ORDER BY'])) {
+            $sql = "SELECT * FROM $table WHERE {$_POST['WHERE']} ORDER BY {$_POST['ORDER BY']};";
+        } else {
+            $sql = "SELECT * FROM $table WHERE {$_POST['WHERE']};";
+        }
+    } else {
+        $sql = "SELECT * FROM $table;";
+    }
+
     $result = $db->query($sql);
 
     if ($format == "json") {
